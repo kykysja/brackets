f2bdf584ef03fd5adca4a1ae516c2115f28e0326
kykysja <kykysja11@gmail.com>
2021-03-05 17:33:38 +0100

    feat: implement the function of the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+
+	bracketsObj = {};
+
+	for (i = 0; i < bracketsConfig.length; i++) {
+
+		bracketsObj[`${bracketsConfig[i][0]}`] = bracketsConfig[i][1];
+	}
+
+	let stack = [];
+	stack.push(str.charAt(0));
+
+	for (i = 1; i < str.length; i++) {
+
+		if (str.charAt(i) === bracketsObj[stack[stack.length - 1]]) {
+			stack.pop();
+
+		} else {
+			stack.push(str.charAt(i));
+		}
+	}
+	return stack.length > 0 ? false : true;
 }
